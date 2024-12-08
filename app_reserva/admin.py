9e385a69fe816b8259da9409e17c96b91a6bbcc5
Gerardo-Ferreira-2022 <gerardo.ferreira@inacapmail.cl>
2024-12-08 13:46:17 -0300

    con static mas form

--- app_reserva/admin.py
+++ app_reserva/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('email', 'fecha', 'estado')
+    list_filter = ('estado', 'fecha')
+    search_fields = ('email', 'descripcion')
--- mysite/settings.py
+++ mysite/settings.py
@@ -142,3 +142,7 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/signin/'
+
+LOGIN_REDIRECT_URL = '/tasks/'
